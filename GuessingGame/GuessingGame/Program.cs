f7c201e74dc32a85dfe62412fe776434acf6bd8b
Magnus Lindberg <magnus.lindberg.chr@gmail.com>
2020-02-22 13:12:57 +0100

    First update
    Just a check that the updates are working

---
+++ GuessingGame/GuessingGame/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GuessingGame
+{
+    class Program
+    {
+        const int maxTries = 3;
+
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
